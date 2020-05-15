59fef7ba8a5702909a3e8992b6a0fc1626b45327
vietle <viet.letranhoang@gmail.com>
2020-05-15 08:32:23 +0300

    added pca for tpot. without pca, takes too much time to run, 0.74 acc with 7 populations

--- find_optimal-pipline.py
+++ find_optimal-pipline.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from tpot import TPOTClassifier
+from sklearn.decomposition import PCA
 
 # %%
 # Load data
@@ -28,7 +29,14 @@ y = y.astype('category').cat.codes
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, train_size=0.75, random_state=42)
 
+# %%
+# PCA
+# %%
+pca = PCA()
+X_train_pca = pca.fit_transform(X_train)
+X_test_pca = pca.transform(X_test)
+
 tpot = TPOTClassifier(verbosity=2, random_state=42)
-tpot.fit(X_train, y_train)
-print(tpot.score(X_test, y_test))
+tpot.fit(X_train_pca, y_train)
+print(tpot.score(X_test_pca, y_test))
 tpot.export('tpot_project_pipeline.py')
